44c9ce0fd27ef380a543219d3c311e144500c564
copiter <glowdonor@gmail.com>
2023-01-04 23:03:25 +0900

    feat : Board 생성, App에 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Board from './pages/Board';
 import CreateTask from './pages/CreateTask';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Board />
       <CreateTask />
     </div>
   );
---
+++ src/pages/Board.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ISSUE_STATE } from '../constants/constants';
+
+
+const Board = () => {
+  return (
+    <article>
+      {ISSUE_STATE.map((item, idx) => {
+        return <section>안뇽</section>;
+      })}
+    </article>
+  );
+};
+
+export default Board;
